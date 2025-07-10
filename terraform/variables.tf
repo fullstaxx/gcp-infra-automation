539806355d4c4062152b06c956a8a493be903358
fullstaxx <idrees.karimzadeh@gmail.com>
2025-07-10 16:33:50 -0400

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,10 @@
 variable "project" {
   description = "The GCP project ID"
+  default     = "gcp-infra-1752174880"
 }
+
 variable "region" {
   default = "us-central1"
 }
+
 variable "db_password" {}
